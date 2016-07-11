a0921cfa75cc7771e0979b0169560acd6f508782
yosefvj <yosefsoj2@gmail.com>
2016-07-12 00:19:31 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -219,7 +219,7 @@ function create_config( )
     "download_media",
     "invite"
     },
-    sudo_users = {67559249,71951412,0,tonumber(our_id)},--Sudo users
+    sudo_users = {128062967(our_id)},--Sudo users
     disabled_channels = {},
     realm = {},--Realms Id
     moderation = {data = 'data/moderation.json'},
